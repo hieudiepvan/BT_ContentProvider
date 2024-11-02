6b8cf1d85c90b98c3f3eeaf52591d4784c3c4c66
hieudiepvan <sdt0354868443@gmail.com>
2024-11-02 22:24:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bt_contentprovider/Adapter/AdapterTinNhan.java
@@ -0,0 +1,46 @@
+package com.example.bt_contentprovider.Adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.bt_contentprovider.Model.TinNhan;
+import com.example.bt_contentprovider.R;
+
+import java.util.List;
+
+public class AdapterTinNhan extends ArrayAdapter<TinNhan> {
+    Activity context;
+    int resource;
+    @NonNull List<TinNhan> objects;
+    public AdapterTinNhan(@NonNull Activity context, int resource, @NonNull List<TinNhan> objects) {
+        super(context, resource, objects);
+
+        this.context = context;
+        this.resource = resource;
+        this.objects = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater layoutInflater = this.context.getLayoutInflater();
+        View row = layoutInflater.inflate(this.resource, null);
+
+        TextView number1 = row.findViewById(R.id.txt_phone);
+        TextView time1 = row.findViewById(R.id.txt_time);
+        TextView body1 = row.findViewById(R.id.txt_body);
+
+        number1.setText(this.objects.get(position).getNumber());
+        time1.setText(this.objects.get(position).getTime());
+        body1.setText(this.objects.get(position).getBody());
+        return row;
+    }
+}
